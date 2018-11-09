9686487bc267312c549b8ac68235c2d0540faad5
Hunter <huntercaouette@gmail.com>
2018-11-08 22:50:18 -0500

    Add files via upload

---
+++ PuzzleView.java
@@ -0,0 +1,22 @@
+package puzzle.boundary;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/** 
+ * Knows how to visually present the puzzle. 
+ */
+public class PuzzleView extends JPanel {
+
+	public PuzzleView() {
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.fillRect(0, 0,  400, 500);
+	}
+	
+}
---
+++ SlidingPuzzleApp.java
@@ -0,0 +1,121 @@
+package puzzle.boundary;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Dimension;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JLabel;
+
+public class SlidingPuzzleApp extends JFrame {
+
+	private JPanel contentPane;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SlidingPuzzleApp frame = new SlidingPuzzleApp();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SlidingPuzzleApp() {
+		setTitle("SlidingPuzzleApp");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GRAY);
+		panel.setSize(new Dimension(400, 500));
+		
+		JButton btnReset = new JButton("Reset");
+		
+		JButton button = new JButton("^");
+		
+		JButton button_1 = new JButton("<");
+		
+		JButton button_2 = new JButton(">");
+		
+		JButton btnV = new JButton("V");
+		
+		JLabel lblMoves = new JLabel("Moves:");
+		
+		JLabel label = new JLabel("12");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 337, GroupLayout.PREFERRED_SIZE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(33)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addGap(38)
+										.addComponent(button)
+										.addGap(36))
+									.addGroup(gl_contentPane.createSequentialGroup()
+										.addComponent(button_1)
+										.addGap(33)
+										.addComponent(button_2)))
+								.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.RELATED, 39, GroupLayout.PREFERRED_SIZE)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(btnReset)
+										.addComponent(btnV))
+									.addGap(15))))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(18)
+							.addComponent(lblMoves)
+							.addGap(18)
+							.addComponent(label)))
+					.addContainerGap(79, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 530, Short.MAX_VALUE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblMoves)
+								.addComponent(label))
+							.addGap(189)
+							.addGroup(gl_contentPane.createSequentialGroup()
+								.addComponent(button)
+								.addPreferredGap(ComponentPlacement.UNRELATED))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(button_2)
+								.addComponent(button_1))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(btnV)
+							.addPreferredGap(ComponentPlacement.RELATED, 213, Short.MAX_VALUE)
+							.addComponent(btnReset)))
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
